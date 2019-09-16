3ff7ff18f0b3baa53ad948f2e378c326c81f2a3f
Peyton Chandarana <s7117@users.noreply.github.com>
2019-09-16 08:36:45 -0400

    Took out se.py

--- program2/program2_env/zd_execute.sh
+++ program2/program2_env/zd_execute.sh
@@ -13,33 +13,11 @@
 ##### Program 2 #####
 ######################################
 
-# COMMAND to Run with Caches
-# You must pass the --caches and --l2cache arguments to initialize the caches.
-# If you don't then you will can't use caches.
-#gem5_X86.opt ./config/se.py --caches --l2cache --l2_size=2MB -c quicksort
-
-######################################
-##### Cache command line options #####
-######################################
-#--caches              
-#--l2cache             
-#--num-dirs=NUM_DIRS   
-#--num-l2caches=NUM_L2CACHES
-#--num-l3caches=NUM_L3CACHES
-#--l1d_size=L1D_SIZE   
-#--l1i_size=L1I_SIZE   
-#--l2_size=L2_SIZE     
-#--l3_size=L3_SIZE     
-#--l1d_assoc=L1D_ASSOC
-#--l1i_assoc=L1I_ASSOC
-#--l2_assoc=L2_ASSOC   
-#--l3_assoc=L3_ASSOC
-######################################
-
 RED='\033[0;31m'
 WHITE='\033[0m'
 BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
+
 ##### USING SIMPLE.PY #####
 cd testing
 # Test Heapsort
@@ -52,28 +30,5 @@ echo -e "${RED}${BOLD}----- QUICKSORT_RUN -----${WHITE}${NORMAL}"
 mv m5out m5out_quicksort
 
 ################################################################################
-
-##### USING SE.PY #####
-#RED='\033[0;31m'
-#WHITE='\033[0m'
-#BOLD=$(tput bold)
-#NORMAL=$(tput sgr0)
-# Must add options to run with different cache sizes.
-#cd testing
-# Test Heapsort
-#echo -e "${RED}${BOLD}----- HEAPSORT_RUN -----${WHITE}${NORMAL}"
-#/usr/local/3rdparty/gem5/build/X86/gem5_X86.opt ./config/se.py --caches \
-# --l2cache --l1i_size="64kB" --l2_size="128kB" -c ./heapsort
-#mv m5out m5out_heapsort
-# Run Quicksort
-#echo -e "${RED}${BOLD}----- QUICKSORT_RUN -----${WHITE}${NORMAL}"
-#/usr/local/3rdparty/gem5/build/X86/gem5_X86.opt ./config/se.py --caches \
-# --l2cache --l1i_size="64kB" --l2_size="128kB" -c ./quicksort
-#mv m5out m5out_quicksort
-
 ################################################################################
 ################################################################################
-################################################################################
-
-
-
